b155c71600e8797149fe1e1028ffdfa46b1f2821
NBL <nabilghoudane@gmail.com>
2020-03-31 08:28:56 +0100

    First commit

---
+++ src/Providers/NamecheapClientApiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Nobilecode\NamecheapClient\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NamecheapClientApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
